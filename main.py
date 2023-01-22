28d3fbad2a42a88ab0e9ad910fbe8ab387087377
Fredericdrnl <fredericdournel@gmail.com>
2023-01-22 21:47:10 +0100

    add bot

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import discord
 from discord.ext import commands
-from random import randint
 import asyncio
 import selectors
+from random import randint
 
 PLAYERS : list[str] = []
 selector = selectors.SelectSelector()
@@ -10,58 +9,67 @@ loop = asyncio.SelectorEventLoop(selector)
 asyncio.set_event_loop(loop)
 
 print("[INFO] Launching bot...")
-client = discord.Bot(intents=discord.Intents.all())
+bot = commands.Bot(command_prefix="!")
 
-@client.event
+@bot.event
 async def on_ready():
     print("[INFO] Bot is ready !")
 
-@client.event
-async def on_message(message):
+@bot.command(name='addMe')
+async def addMe(message):
+    PLAYERS.append(str(message.author))
+    print(PLAYERS)
+    await message.channel.send(f"{message.author.mention} added")
+
+@bot.command(name="addPlayer")
+async def addPlayer(ctx, player):
+    PLAYERS.append(str(player.display_name))
+    await ctx.channel.send(f"{player.display_name} added")
+
+@bot.command(name="removePlayer")
+async def removePlayer(ctx, player):
+    PLAYERS.pop(player.display_name.index())
+    await ctx.channel.send(f"{player.display_name} removed")
+
+@bot.command(name="checkList")
+async def checkList(ctx):
+    if len(PLAYERS) == 0:
+        await ctx.channel.send("No one on list")
+    for player in PLAYERS:
+        await ctx.channel.send(player)
+
+@bot.command(name="clearList")
+async def clearList(ctx):
+    PLAYERS.clear()
+    await ctx.channel.send("list cleared")
+
+@bot.command(name="pugs")
+async def pugs(ctx):
 
     Team1 : list[str] = []
     Team2 : list[str] = []
     rand : int
     joueur : str
 
-    if message.content.lower() == "join pugs":
-        PLAYERS.append(str(message.author))
-        print(PLAYERS)
-        await message.channel.send(f"{message.author.mention} added")
+    if len(PLAYERS) < 9:
+        await ctx.channel.send("Pas assez de joueur pour PUGS")
+    else:
+        for i in range(len(PLAYERS) // 2):
+            rand = randint(0, len(PLAYERS) -1)
+            joueur = PLAYERS.pop(rand)
+            Team1.append(joueur)
+            rand = randint(0, len(PLAYERS) -1)
+            joueur = PLAYERS.pop(rand)
+            Team2.append(joueur)
 
-    if message.content.lower() == "check players":
-        if len(PLAYERS) == 0:
-            await message.channel.send("No one on list")
-        for player in PLAYERS:
-            await message.channel.send(player)
+        await ctx.channel.send("\n====== TEAM 1 ======")
+        for player in Team1:
+            await ctx.channel.send("-" + player)
 
-    if message.content.lower() == "clear players":     
-        PLAYERS.clear()
-        await message.channel.send("list cleared")
-
-    if message.content.lower() == "go pugs":
-        if len(PLAYERS) < 9:
-            await message.channel.send("Pas assez de joueur pour PUGS")
-        else:
-            for i in range(len(PLAYERS) // 2):
-                rand = randint(0, len(PLAYERS) -1)
-                joueur = PLAYERS.pop(rand)
-                Team1.append(joueur)
-                rand = randint(0, len(PLAYERS) -1)
-                joueur = PLAYERS.pop(rand)
-                Team2.append(joueur)
-
-            await message.channel.send("\n====== TEAM 1 ======")
-            for player in Team1:
-                await message.channel.send("-" + player)
-
-            await message.channel.send("\n====== TEAM 2 ======")
-            for player in Team2:
-                await message.channel.send("-" + player)
-
-            await message.channel.send("\n\nGL !")
-        PLAYERS.clear()   
+        await ctx.channel.send("\n====== TEAM 2 ======")
+        for player in Team2:
+            await ctx.channel.send("-" + player)
+        await ctx.channel.send("\n\nGL !")
 
 
-
-client.run(open("token.txt", "r").readline())
+bot.run(open("token.txt", "r").readline())
