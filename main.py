eddc574d85d9fafb3cadc1688c7d82067a5725ca
Fredericdrnl <fredericdournel@gmail.com>
2023-01-23 23:11:02 +0100

    class test

--- main.py
+++ main.py
@@ -3,73 +3,68 @@ import asyncio
 import selectors
 from random import randint
 
-PLAYERS : list[str] = []
 selector = selectors.SelectSelector()
 loop = asyncio.SelectorEventLoop(selector)
 asyncio.set_event_loop(loop)
 
-print("[INFO] Launching bot...")
-bot = commands.Bot(command_prefix="!")
+class PugsBot(commands.Bot):
+    def __init__(self):
+        super().__init__(command_prefix=".")
+        self.players : list[str] = []
+        self.team1 : list[str] = []
+        self.team2 : list[str] = []
+        self.rand : int
+        self.joueur : str
 
-@bot.event
-async def on_ready():
-    print("[INFO] Bot is ready !")
+        @self.command(name='addMe')
+        async def addMe(self, ctx):
+            self.players.append(str(ctx.author))
+            print(self.players)
+            await ctx.channel.send(f"{ctx.author.mention} added")
 
-@bot.command(name='addMe')
-async def addMe(message):
-    PLAYERS.append(str(message.author))
-    print(PLAYERS)
-    await message.channel.send(f"{message.author.mention} added")
+    @commands.command(name="addPlayer")
+    async def addPlayer(self, ctx):
+        self.players.append(str(ctx.author.name))
+        await ctx.channel.send(f"{ctx.author.name} added")
 
-@bot.command(name="addPlayer")
-async def addPlayer(ctx, player):
-    PLAYERS.append(str(player.display_name))
-    await ctx.channel.send(f"{player.display_name} added")
+    @commands.command(name="removePlayer")
+    async def removePlayer(self, ctx, player):
+        self.players.pop(player.display_name.index())
+        await ctx.channel.send(f"{player.display_name} removed")
 
-@bot.command(name="removePlayer")
-async def removePlayer(ctx, player):
-    PLAYERS.pop(player.display_name.index())
-    await ctx.channel.send(f"{player.display_name} removed")
+    @commands.command(name="checkList")
+    async def checkList(self, ctx):
+        if len(self.players) == 0:
+            await ctx.channel.send("No one on list")
+        for player in self.players:
+            await ctx.channel.send(player)
 
-@bot.command(name="checkList")
-async def checkList(ctx):
-    if len(PLAYERS) == 0:
-        await ctx.channel.send("No one on list")
-    for player in PLAYERS:
-        await ctx.channel.send(player)
+    @commands.command(name="clearList")
+    async def clearList(self, ctx):
+        self.players.clear()
+        await ctx.channel.send("list cleared")
 
-@bot.command(name="clearList")
-async def clearList(ctx):
-    PLAYERS.clear()
-    await ctx.channel.send("list cleared")
+    @commands.command(name="pugs")
+    async def pugs(self, ctx):
+        if len(self.players) < 9:
+            await ctx.channel.send("Pas assez de joueur pour PUGS")
+        else:
+            for i in range(len(self.players) // 2):
+                rand = randint(0, len(self.players) -1)
+                joueur = self.players.pop(rand)
+                self.team1.append(joueur)
+                rand = randint(0, len(self.players) -1)
+                joueur = self.players.pop(rand)
+                self.team2.append(joueur)
 
-@bot.command(name="pugs")
-async def pugs(ctx):
+            await ctx.channel.send("\n====== TEAM 1 ======")
+            for player in self.team1:
+                await ctx.channel.send("-" + player)
 
-    Team1 : list[str] = []
-    Team2 : list[str] = []
-    rand : int
-    joueur : str
+            await ctx.channel.send("\n====== TEAM 2 ======")
+            for player in self.team2:
+                await ctx.channel.send("-" + player)
+            await ctx.channel.send("\n\nGL !")
 
-    if len(PLAYERS) < 9:
-        await ctx.channel.send("Pas assez de joueur pour PUGS")
-    else:
-        for i in range(len(PLAYERS) // 2):
-            rand = randint(0, len(PLAYERS) -1)
-            joueur = PLAYERS.pop(rand)
-            Team1.append(joueur)
-            rand = randint(0, len(PLAYERS) -1)
-            joueur = PLAYERS.pop(rand)
-            Team2.append(joueur)
-
-        await ctx.channel.send("\n====== TEAM 1 ======")
-        for player in Team1:
-            await ctx.channel.send("-" + player)
-
-        await ctx.channel.send("\n====== TEAM 2 ======")
-        for player in Team2:
-            await ctx.channel.send("-" + player)
-        await ctx.channel.send("\n\nGL !")
-
-
+bot = PugsBot()
 bot.run(open("token.txt", "r").readline())
