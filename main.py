9ce21413402ba012c99ef7aa5151ad122b047ce2
Fredericdrnl <114562911+Fredericdrnl@users.noreply.github.com>
2023-01-25 09:30:05 +0100

    fix function

--- main.py
+++ main.py
@@ -4,14 +4,13 @@ from random import randint
 import asyncio
 import selectors
 
-PLAYERS : list[str] = []
-ON_DISCORD : list[int] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 selector = selectors.SelectSelector()
 loop = asyncio.SelectorEventLoop(selector)
 asyncio.set_event_loop(loop)
 
 print("[INFO] Launching bot...")
-client = discord.Bot(intents=discord.Intents.all())
+PLAYERS : list[str] = []
+client = discord.Client(intents=discord.Intents.all())
 
 @client.event
 async def on_ready():
@@ -27,7 +26,8 @@ async def on_message(message):
 
     # Le joueur qui écrit le message s'inscrit au pugs.
     if message.content.lower() == "!join pugs" or message.content.lower() == "!jpu":
-        PLAYERS.append(str(message.author))
+        id : str = "<@" + str(message.author.id) + ">"
+        PLAYERS.append(id)
         print(PLAYERS)
         await message.channel.send(f"<@{message.author.id}> added")
 
@@ -45,18 +45,18 @@ async def on_message(message):
         else:
             await message.channel.send("Voici la liste des joueurs inscrits :")
             for player in PLAYERS:
-                await message.channel.send("-" + player)
+                await message.channel.send("- " + player)
 
-    #supprimer un joueur de la liste
-    if message.content.lower() == "!remove players" or message.content.lower() == "!rp":
+    #supprimer un joueur de la liste (le supprime une seule fois).
+    if "!remove players" in message.content.lower()  or "!rp" in message.content.lower():
         for user_mentioned in message.mentions:
-            id : str = "<@" + str(user_mentioned.id) + ">"
-            if id in PLAYERS:
-                PLAYERS.pop(PLAYERS.index(id))
-                await message.channel.send("Le joueur" + id + "à été supprimé de la liste.")
+            user : str = str(user_mentioned)
+            print(user)
+            if user in PLAYERS:
+                PLAYERS.pop(PLAYERS.index(user))
+                await message.channel.send("Le joueur " + str(user) + " à été supprimé de la liste.")
             else:
-                await message.channel.send("Le joueur" + id + "n'est pas dans la liste.")    
-
+                await message.channel.send("Le joueur " + str(user) + " n'est pas dans la liste.")    
 
     # Supprimer les éléments de la liste des participants.
     if message.content.lower() == "!clear players" or message.content.lower() == "!clp":     
@@ -85,24 +85,23 @@ async def on_message(message):
                 await message.channel.send("- " + player)
             await message.channel.send("\n\nGL !")
 
-        if message.content.lower() == "!teams discord" or message.content.lower() == "!td":
-            for i in range(len(ON_DISCORD) // 2):
-                rand = randint(0, len(ON_DISCORD) -1)
-                joueur = ON_DISCORD.pop(rand)
-                Team1.append(joueur)
-                rand = randint(0, len(ON_DISCORD -1))
-                joueur = ON_DISCORD.pop(rand)
-                Team2.append(joueur)
+    if message.content.lower() == "!teams discord" or message.content.lower() == "!td":
+        on_discord : list[int] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        for i in range(len(on_discord) // 2):
+            rand = randint(0, len(on_discord) -1)
+            joueur = on_discord.pop(rand)
+            Team1.append(joueur)
+            rand = randint(0, len(on_discord) - 1)
+            joueur = on_discord.pop(rand)
+            Team2.append(joueur)
 
-            await message.channel.send("\n====== TEAM 1 ======")
-            for player in Team1:
-                await message.channel.send("- " + player)
+        await message.channel.send("\n====== TEAM 1 ======")
+        for player in Team1:
+            await message.channel.send("- " + str(player))
 
-            await message.channel.send("\n====== TEAM 2 ======")
-            for player in Team2:
-                await message.channel.send("- " + player)
-            await message.channel.send("\n\nGL !")
-            ON_DISCORD = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
+        await message.channel.send("\n====== TEAM 2 ======")
+        for player in Team2:
+            await message.channel.send("- " + str(player))
+        await message.channel.send("\n\nGL !")
 
 client.run(open("token.txt", "r").readline())
